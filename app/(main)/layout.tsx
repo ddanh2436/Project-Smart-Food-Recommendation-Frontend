1f58cf8176baea2792950d1b0981e0b312aa5627
Đào Duy Anh <131458303+Daoduyanh2412@users.noreply.github.com>
2025-11-17 21:33:04 +0700

    Add Footer Component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,10 @@
-// ddanh2436/project-smart-food-recommendation-frontend/Project-Smart-Food-Recommendation-Frontend-1fc724bd73b2d99b6b7202f40f81236938357594/app/(main)/layout.tsx
+// app/(main)/layout.tsx
 
-"use client"; 
+"use client";
 
 import Header from "@/components/Header/Header";
-import "@/components/Header/Header.css"; 
+import Footer from "@/components/Footer/Footer"; // <--- Import Footer
+import "@/components/Header/Header.css";
 import { usePathname } from 'next/navigation';
 
 export default function MainLayout({
@@ -12,19 +13,20 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-  
+
   // Ẩn Header nếu đường dẫn bắt đầu bằng /profile
-  const hideHeader = pathname.startsWith('/profile'); 
-  
+  const hideHeader = pathname.startsWith('/profile');
+
   return (
     <>
-      {/* Hiển thị Header nếu không phải trang Profile */}
-      {!hideHeader && <Header />} 
-      
-      {/* Điều chỉnh padding top */}
-      <main style={{ paddingTop: hideHeader ? '0' : '81px' }}> 
+      {!hideHeader && <Header />}
+
+      <main style={{ paddingTop: hideHeader ? '0' : '0' }}>
         {children}
       </main>
+
+      {/* Thêm Footer vào đây */}
+      <Footer />
     </>
   );
 }
